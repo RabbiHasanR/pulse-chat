69209d3bccbea6900c6d687f8c39d84672feba7e
rabbi <jasrabbi50@gmail.com>
2026-01-06 23:44:59 +0600

    feat(chat): add [test_field] field to MediaAsset model

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
-# set -e
-# until nc -z $DATABASE_HOST $DATABASE_PORT; do
-#   echo "Waiting for Postgres at $DATABASE_HOST:$DATABASE_HOST..."
-#   sleep 1
-# done
-
-# python manage.py makemigrations users
-# python manage.py migrate users
-
-# python manage.py makemigrations
-# python manage.py migrate --noinput
-
-# exec "$@"
-
 set -e
-
 until nc -z $DATABASE_HOST $DATABASE_PORT; do
-  echo "Waiting for Postgres at $DATABASE_HOST:$DATABASE_PORT..."
+  echo "Waiting for Postgres at $DATABASE_HOST:$DATABASE_HOST..."
   sleep 1
 done
 
-# REMOVE 'makemigrations' lines. 
-# We only apply existing migrations now.
+python manage.py makemigrations users
+python manage.py migrate users
+
+python manage.py makemigrations
 python manage.py migrate --noinput
 
 exec "$@"
