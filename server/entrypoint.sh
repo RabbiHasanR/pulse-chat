5c245aac81783bf8b20454dc592b19397e2ee31e
rabbi <jasrabbi50@gmail.com>
2026-01-06 23:23:37 +0600

    fix: add missing migration files

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
+# set -e
+# until nc -z $DATABASE_HOST $DATABASE_PORT; do
+#   echo "Waiting for Postgres at $DATABASE_HOST:$DATABASE_HOST..."
+#   sleep 1
+# done
+
+# python manage.py makemigrations users
+# python manage.py migrate users
+
+# python manage.py makemigrations
+# python manage.py migrate --noinput
+
+# exec "$@"
+
 set -e
+
 until nc -z $DATABASE_HOST $DATABASE_PORT; do
-  echo "Waiting for Postgres at $DATABASE_HOST:$DATABASE_HOST..."
+  echo "Waiting for Postgres at $DATABASE_HOST:$DATABASE_PORT..."
   sleep 1
 done
 
-python manage.py makemigrations users
-python manage.py migrate users
-
-python manage.py makemigrations
+# REMOVE 'makemigrations' lines. 
+# We only apply existing migrations now.
 python manage.py migrate --noinput
 
 exec "$@"
