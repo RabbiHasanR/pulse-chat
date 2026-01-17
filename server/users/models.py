743e98c1d3d070da8e5282f5f7778bdf848d30ff
rabbi <jasrabbi50@gmail.com>
2026-01-17 23:16:34 +0600

    feat(models): add avatar_bucket and avatar_key to ChatUser model

--- server/users/models.py
+++ server/users/models.py
@@ -7,9 +7,25 @@ class ChatUser(AbstractUser):
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=150)
     is_admin = models.BooleanField(default=False)
+    
+    # Store S3 metadata instead of hardcoded URL
+    avatar_bucket = models.CharField(max_length=100, blank=True, null=True)
+    avatar_key = models.CharField(max_length=255, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'full_name']
+
+    @property
+    def avatar_url(self):
+        """
+        Dynamically constructs the S3 URL.
+        Returns None if key or bucket is missing.
+        """
+        if self.avatar_bucket and self.avatar_key:
+            # Standard S3 URL format. 
+            # If you use a custom domain or CloudFront, change this string.
+            return f"https://{self.avatar_bucket}.s3.amazonaws.com/{self.avatar_key}"
+        return None
 
 
 
