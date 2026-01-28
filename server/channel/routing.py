24690d408d209072283e087daaa63caf00838115
rabbi <jasrabbi50@gmail.com>
2026-01-28 23:10:12 +0600

    refactor(channel): Simplify WebSocket URL routing

--- server/channel/routing.py
+++ server/channel/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/user/(?P<user_id>\d+)/$', consumers.UserSocketConsumer.as_asgi()),
+    re_path(r'ws/chat/$', consumers.UserSocketConsumer.as_asgi()),
 ]
